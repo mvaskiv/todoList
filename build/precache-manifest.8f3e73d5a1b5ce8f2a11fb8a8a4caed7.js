75eab95a585dcf96895295e27ba49c210904f3b4
mvaskiv <mike@vaskiv.com>
2018-12-10 18:33:07 +0200

    build

---
+++ build/precache-manifest.8f3e73d5a1b5ce8f2a11fb8a8a4caed7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "473f201cf81dd4b224d9",
+    "url": "/static/js/main.473f201c.chunk.js"
+  },
+  {
+    "revision": "a06ab5e159fa09ea61a6",
+    "url": "/static/js/1.a06ab5e1.chunk.js"
+  },
+  {
+    "revision": "473f201cf81dd4b224d9",
+    "url": "/static/css/main.c2659d62.chunk.css"
+  },
+  {
+    "revision": "14aadbfa537be0ecc81b98f66d1a3f48",
+    "url": "/index.html"
+  }
+];
